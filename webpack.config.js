f9726b1c1f47ce93a2af4cc7f1b3b6e6b74213fb
Rolf Bansbach <rolfisub@gmail.com>
2018-07-16 14:43:35 -0400

    setting up build

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require("path");
+module.exports = {
+  entry: "./src/index.ts",
+  mode:"production",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "fsv.js",
+    library: "fsv",
+    libraryTarget: "umd"
+  }
+};
