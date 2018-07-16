8974c0f579b3328861086ed7ab83ddc04b7fe124
Rolf Bansbach <rolfisub@gmail.com>
2018-07-16 12:47:08 -0400

    adding config files

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    printWith: 120,
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'none',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    arrowParens: 'always',
+};
