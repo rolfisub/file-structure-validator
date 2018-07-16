7947ea62ff2b1b439f6d94c87e3eee8b26731f90
Rolf Bansbach <rolf@paperstreetmedia.com>
2018-07-16 15:26:57 -0400

    settin gup first build

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('index.ts');
+interface ConfigObject {
+    files: string[];
+    dirs: string[];
+}
+
+interface Error {
+    valid: boolean;
+    msg: string;
+}
+
+export default class FSV {
+    constructor(public config: ConfigObject) {}
+
+    public validate(): Error {
+        return {
+            valid: true,
+            msg: ''
+        };
+    }
+}
