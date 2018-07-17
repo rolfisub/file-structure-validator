96d2ffa2fc51d22a247ee62cbffacf21494c0ed1
Rolf Bansbach <rolfisub@gmail.com>
2018-07-17 10:54:22 -0400

    initial set of tests

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,47 @@
+import * as path from 'path';
+import * as fs from 'fs';
+
 interface ConfigObject {
+    base: string;
     files: string[];
     dirs: string[];
 }
 
-interface Error {
+interface ValidationObject {
     valid: boolean;
     msg: string;
 }
 
 export default class FSV {
-    constructor(public config: ConfigObject) {}
+    constructor(public config: ConfigObject) {
+        this.validate = this.validate.bind(this);
+    }
 
-    public validate(): Error {
+    public validate(): ValidationObject {
+        const dir = path.resolve(this.config.base);
+        //validate that base path exists
+        if (!fs.existsSync(dir)) {
+            return {
+                valid: false,
+                msg: 'base path does not exists: ' + dir
+            };
+        }
+
+        const fileResult: ValidationObject = {
+            valid: true,
+            msg: ''
+        };
+        this.config.files.forEach((file) => {
+            const fullFilePath = path.resolve(dir, file);
+            if(fileResult.valid && !fs.existsSync(fullFilePath)) {
+                fileResult.valid = false;
+                fileResult.msg = "file does not exists: " + fullFilePath
+            }
+        });
+        if(!fileResult.valid) {
+            return fileResult;
+        }
+
         return {
             valid: true,
             msg: ''
--- src/tests/test.ts
+++ src/tests/test.ts
@@ -2,13 +2,65 @@ import fsval from "../index";
 import { expect } from 'chai';
 
 describe('file structure validator', ()=> {
-    const fsv = new fsval({
-      files:[''],
-      dirs: ['']
-    });
+  const fsv1 = new fsval({
+    base: "./src/tests/dirtestaaa",
+    files:[],
+    dirs: []
+  });
 
-    it('should return valid true', ()=> {
-      expect(fsv.validate()).to.have.property('valid').to.equal(true);
-    })
+  it('base doesnt exists', ()=> {
+    expect(fsv1.validate()).to.have.property('valid').to.equal(false);
+  });
+
+  const fsv2 = new fsval({
+    base: "./src/tests/dirtest",
+    files:[],
+    dirs: []
+  });
+
+  it('base works', ()=> {
+    expect(fsv2.validate()).to.have.property('valid').to.equal(true);
+  });
+
+  const fsv3 = new fsval({
+    base: "./src/tests/dirtest",
+    files:['testaaa.txt'],
+    dirs: []
+  });
+
+  it('file doesnt exist', ()=> {
+    expect(fsv3.validate()).to.have.property('valid').to.equal(false);
+  });
+
+  const fsv4 = new fsval({
+    base: "./src/tests/dirtest",
+    files:['test.txt'],
+    dirs: []
+  });
+
+  it('file exist', ()=> {
+    expect(fsv4.validate()).to.have.property('valid').to.equal(true);
+  });
+
+  const fsv5 = new fsval({
+    base: "./src/tests/dirtest",
+    files:['test.txt', 'testaaaa.txt'],
+    dirs: []
+  });
+
+  it('one file does not exist', ()=> {
+    expect(fsv5.validate()).to.have.property('valid').to.equal(false);
+  });
+
+  const fsv6 = new fsval({
+    base: "./src/tests/dirtest",
+    files:['test.txt', 'test2.txt'],
+    dirs: []
+  });
+
+  it('multiple files exists', ()=> {
+    expect(fsv6.validate()).to.have.property('valid').to.equal(true);
+  });
+
 });
 
